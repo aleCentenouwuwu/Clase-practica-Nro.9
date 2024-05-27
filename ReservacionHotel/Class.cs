734ea9971c4888a923b02427b79af7d6e331c7bd
AlexaniaCenteno <alexaniapauth@gmail.com>
2024-05-27 11:48:07 -0600

    Cambios

--- ReservacionHotel/Class.cs
+++ ReservacionHotel/Class.cs
@@ -6,9 +6,72 @@ using System.Threading.Tasks;
 
 namespace ReservacionHotel
 {
-    public class Reservacion
+    public class ReservacionHotel
     {
-        string nombre;
-        int numerohabitacion;
+        public string nombre;
+        public int numerohabitacion;
+        public void HacerReservacion()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Que habitacion desea reservar?");
+            numerohabitacion =Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Su reservacion se ha realizado con exito");
+            Console.ReadKey();
+        }
+        public void CancelarReservacion()
+        {
+           
+            Console.WriteLine(">> Cancelar reservacion");
+            Console.WriteLine("¿Su reservación es la siguiente?");
+            Console.WriteLine($"Nombre del huesped: {this.nombre}.");
+            Console.WriteLine($"Numero habitacion: {this.numerohabitacion}");
+            Console.WriteLine("");
+            Console.WriteLine("1. Si");
+            Console.WriteLine("2. No");
+            int opc =Convert.ToInt32(Console.ReadLine());
+            if (opc ==1)
+            {
+                Console.Clear();
+                Console.WriteLine("Desea cancelar su reservacion? ");
+                Console.WriteLine("");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No");
+                byte opcion =Convert.ToByte(Console.ReadLine());
+                if (opcion ==1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Su reservacion se ha cancelado con exito");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Su reservacion sigue activa, gracias por darnos plata.");
+                    Console.ReadKey();
+                }
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("oki, bye :33");
+                Console.ReadKey();
+            }
+        }
+        public void ConsultarReservacion(bool Verificacion)
+        {
+            if(Verificacion == true)
+            {
+                Console.WriteLine($"La habitacion {numerohabitacion} esta reservada por {nombre}");
+
+            }
+            else
+            {
+                Console.WriteLine("No hay reservacion a su nombre");
+            }
+          
+        }
     }
 }
--- ReservacionHotel/Program.cs
+++ ReservacionHotel/Program.cs
@@ -10,6 +10,48 @@ namespace ReservacionHotel
     {
         static void Main(string[] args)
         {
+            bool Salir = false;
+            do
+            {
+                Console.WriteLine("-------Opciones de reservacion-------");
+                Console.WriteLine("");
+                Console.WriteLine("1. Hacer reservacion");
+                Console.WriteLine("2. Consultar reservacion");
+                Console.WriteLine("3. Cancelar reservacion");
+                Console.WriteLine("4. Salir del menu");
+                byte opc = Convert.ToByte(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        Console.Clear();
+                        ReservacionHotel Hacer = new ReservacionHotel();
+                        Hacer.HacerReservacion();
+                        Hacer.nombre = "Norvin";
+                        Hacer.numerohabitacion =1;
+                        break;
+                    case 2:
+                        Console.Clear();
+                        ReservacionHotel Consultar = new ReservacionHotel();
+                        //Consultar.ConsultarReservacion();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        ReservacionHotel Cancelar = new ReservacionHotel();
+                        Cancelar.CancelarReservacion();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("ly ");
+                        Salir = true;
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+
+
+                }
+            } while (!Salir);
         }
     }
 }
