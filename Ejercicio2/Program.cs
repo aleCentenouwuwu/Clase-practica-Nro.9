2cd87cad709d893500f22c67aebad020352165bc
LiaMatamoros <liabetzayda154@gmail.com>
2024-05-27 11:40:44 -0600

    Ejercio 2 terminado(revisar no sirve)

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ejercicio2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Biblioteca Alexandría = new Biblioteca();
+
+            Console.WriteLine("Buenos dias, en que le puedo ayudar");
+            Console.WriteLine("1. Buscar un libro");
+            Console.WriteLine("2. Devolver un libro");
+
+            byte opc = Convert.ToByte(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    Console.WriteLine("Que libro quiere prestar");
+                    Alexandría.libro = Console.ReadLine();
+
+                    bool Disponible = Convert.ToBoolean(Alexandría.Consultar());
+                    if (Disponible==true)
+                    {
+                        Console.WriteLine("Quiere prestar el libro");
+                        Console.WriteLine("1. Si");
+                        Console.WriteLine("2. No");
+                        byte opc2= Convert.ToByte(Console.ReadLine());
+                        switch (opc2)
+                        {
+                            case 1:
+                                Console.WriteLine("Digite su nombre");
+                                Alexandría.usuario = Console.ReadLine();
+                                Alexandría.Prestar();
+                                break;
+                            case 2:
+                                Console.WriteLine("A, ok. Chao pescao");
+                                break;
+                            default:
+                                Console.WriteLine("Opción Invalida");
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Cual es su nombre");
+                    string usuario = Console.ReadLine();
+
+                    Console.WriteLine("Cual libro devolverá");
+                    string libro = Console.ReadLine();
+
+                    if (usuario == Alexandría.usuario&& libro == Alexandría.libro)
+                    {
+                        Alexandría.Devolver();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opción Invalida");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
