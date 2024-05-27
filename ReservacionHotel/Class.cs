edbd3c9973a29de47a5b4ace72b8576489fcc0d2
AlexaniaCenteno <alexaniapauth@gmail.com>
2024-05-27 10:16:32 -0600

    Agregar archivos de proyecto.

---
+++ ReservacionHotel/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservacionHotel
+{
+    public class Reservacion
+    {
+        string nombre;
+        int numerohabitacion;
+    }
+}
