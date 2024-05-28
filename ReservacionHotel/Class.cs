f7237ee850592edfd529c055a6f69f0203b29782
Nate2066 <fredericknat1522@gmail.com>
2024-05-27 21:22:17 -0500

    Ejercicio reservacion de un hotel, construido desde 0

--- ReservacionHotel/Class.cs
+++ ReservacionHotel/Class.cs
@@ -1,78 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace ReservacionHotel
 {
     public class ReservacionHotel
     {
         public string nombre;
-        public int numerohabitacion;
+        public int numeroHabitacion;
+
         public void HacerReservacion()
         {
-            Console.WriteLine("Ingrese su nombre");
+            Console.WriteLine("----------Hacer una reservacion----------");
+            Console.WriteLine("");
+            Console.WriteLine("-Ingrese su nombre-");
             nombre = Console.ReadLine();
-            Console.WriteLine("Que habitacion desea reservar?");
-            numerohabitacion =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("-Ingrese el numero de habitacion-");
+            numeroHabitacion = Convert.ToInt32(Console.ReadLine());
+
             Console.Clear();
             Console.WriteLine("Su reservacion se ha realizado con exito");
-            Console.ReadKey();
         }
         public void CancelarReservacion()
         {
-           
-            Console.WriteLine(">> Cancelar reservacion");
-            Console.WriteLine("¿Su reservación es la siguiente?");
-            Console.WriteLine($"Nombre del huesped: {nombre}.");
-            Console.WriteLine($"Numero habitacion: {numerohabitacion}");
+            Console.WriteLine("------Su reservacion es la siguiente?------");
             Console.WriteLine("");
+            Console.WriteLine($"Nombre de huesped: {nombre}");
+            Console.WriteLine($"Numero de habitacion {numeroHabitacion}");
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------------");
             Console.WriteLine("1. Si");
             Console.WriteLine("2. No");
-            int opc =Convert.ToInt32(Console.ReadLine());
-            if (opc ==1)
+            byte opc = Convert.ToByte(Console.ReadLine());
+
+            if( opc == 1 ) 
             {
                 Console.Clear();
-                Console.WriteLine("Desea cancelar su reservacion? ");
+                Console.WriteLine("Desea cancelar la reservacion?");
                 Console.WriteLine("");
                 Console.WriteLine("1. Si");
                 Console.WriteLine("2. No");
-                byte opcion =Convert.ToByte(Console.ReadLine());
-                if (opcion ==1)
+                byte opc2 = Convert.ToByte(Console.ReadLine());
+
+                if(opc2 == 1)
+                {
+                    //Un "default" como valor de una variable refleja su estado original
+                    //En caso de que se le asigne a una variable int, esta obtendra el valor de 0
+                    nombre = default;
+                    numeroHabitacion = default;
+
+                    Console.Clear();
+                    Console.WriteLine("Su cancelacion se ha realizado con exito");
+                }
+                else if (opc2 == 2)
                 {
                     Console.Clear();
-                    Console.WriteLine("Su reservacion se ha cancelado con exito");
-                    Console.ReadKey();
+                    Console.WriteLine("...");
                 }
                 else
                 {
-                    Console.Clear();
-                    Console.WriteLine("Su reservacion sigue activa, gracias por darnos plata.");
-                    Console.ReadKey();
+                    Console.WriteLine("Opcion invalida");
                 }
-
+            }
+            else if ( opc == 2 )
+            {
+                Console.Clear();
+                Console.WriteLine("...");
             }
             else
             {
                 Console.Clear();
-                Console.WriteLine("oki, bye :33");
-                Console.ReadKey();
+                Console.WriteLine("Opcion invalida");
             }
-        }
-       public bool ConsultarReservacion(bool Verif)
-        {
-            
-            if(Verif == true)
-            {
-                Console.WriteLine($"La habitacion {numerohabitacion} esta reservada por {nombre}");
 
-            }
-            else
-            {
-                Console.WriteLine("No hay reservacion a su nombre");
-            }
-            return true;
+        }
+        public void MostrarReservacion()
+        {
+            Console.WriteLine("---------------Reservacion-----------------");
+            Console.WriteLine("");
+            Console.WriteLine($"Nombre de huesped: {nombre}");
+            Console.WriteLine($"Numero de habitacion: {numeroHabitacion}");
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------------");
         }
     }
 }
--- ReservacionHotel/Program.cs
+++ ReservacionHotel/Program.cs
@@ -1,61 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.Linq.Expressions;
 
 namespace ReservacionHotel
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-
-            // El error es que creaban un objeto por cada accion, entonces recuerden que solo es un objeto 
-            // es el que tiene su informacion, si crean otro este estara en blanco
-            ReservacionHotel reservacion = new ReservacionHotel();  
+            ReservacionHotel reservacion = new ReservacionHotel();
             bool Salir = false;
             do
             {
-                Console.WriteLine("-------Opciones de reservacion-------");
-                Console.WriteLine("");
-                Console.WriteLine("1. Hacer reservacion");
-                Console.WriteLine("2. Consultar reservacion");
-                Console.WriteLine("3. Cancelar reservacion");
-                Console.WriteLine("4. Salir del menu");
-                Console.WriteLine("-------------------------------------");
-                byte opc = Convert.ToByte(Console.ReadLine());
-
-                switch (opc)
+                try
                 {
-                    case 1:
-                        Console.Clear();
-                       
-                        reservacion.HacerReservacion();
-                       
-                        break;
-                    case 2:
-                        Console.Clear();
-                        reservacion.ConsultarReservacion();
-                        break;
-                    case 3:
-                        Console.Clear();
-                      
-                        reservacion.CancelarReservacion();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        Console.WriteLine("lysm uwu");
-                        Salir = true;
-                        Console.ReadKey();
-                        break;
-                    default:
-                        Console.WriteLine("Opcion no valida, intente de nuevo");
-                        break;
+                    Console.Clear();
+                    Console.WriteLine("----------Elija una opcion----------");
+                    Console.WriteLine("");
+                    Console.WriteLine("1.     Hacer reservacion");
+                    Console.WriteLine("2.     Cancelar reservacion");
+                    Console.WriteLine("3.     Mostrar reservacion");
+                    Console.WriteLine("4.     Salir");
+                    Console.WriteLine("------------------------------------");
+                    byte opc = Convert.ToByte(Console.ReadLine());
 
-
+                    switch (opc)
+                    {
+                        case 1:
+                            Console.Clear();
+                            if (reservacion.nombre != default && reservacion.numeroHabitacion != default)
+                            {
+                                Console.WriteLine("Usted ya realizo una reservacion");
+                            }
+                            else
+                            {  
+                                reservacion.HacerReservacion();
+                            }
+                              
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            if (reservacion.nombre != default && reservacion.numeroHabitacion != default)
+                            { 
+                                reservacion.CancelarReservacion();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Lo sentimos! no encontramos ninguna reservacion a su nombre");
+                            }
+                           
+                            Console.ReadKey();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            if (reservacion.nombre != default && reservacion.numeroHabitacion != default)
+                            {
+                                reservacion.MostrarReservacion();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Lo sentimos! no encontramos ninguna reservacion a su nombre");
+                            }
+                            Console.ReadKey();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Salir = true;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Error! Valor invalido");
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.Clear();
+                    reservacion.nombre = default;
+                    reservacion.numeroHabitacion = default;
+                    Console.WriteLine("Error! Valor invalido");
+                    Console.ReadKey();
+                }
+                catch (FormatException)
+                {
+                    reservacion.nombre = default;
+                    reservacion.numeroHabitacion = default;
+                    Console.Clear();
+                    Console.WriteLine("Error! Valor invalido");
+                    Console.ReadKey();
                 }
             } while (!Salir);
+            
         }
     }
 }
